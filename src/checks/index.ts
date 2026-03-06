f132a7c490a72e878f8fca3845d589fd5288f407
Nicholas Rodrigues Lordello <n@lordello.net>
2026-03-06 16:03:10 +0100

    Reflect Changes to Baseline Checks (#12)
    They were recently changed in the validator, so the payout scripts needs
    to support the changes as well.

--- src/checks/index.ts
+++ src/checks/index.ts
@@ -12,7 +12,7 @@ import {
 	targets,
 } from "./combinators.js";
 
-const ALLOWED = {
+const SUPPORTED = {
 	fallbackHandlers: new Set<Address>([
 		zeroAddress,
 		"0x85a8ca358D388530ad0fB95D0cb89Dd44Fc242c3",
@@ -23,14 +23,17 @@ const ALLOWED = {
 		"0x017062a1dE2FE6b99BE3d9d37841FeD19F573804",
 	]),
 	guards: new Set<Address>([zeroAddress]),
-	modules: new Set<Address>([]),
+	modules: new Set<Address>([
+		"0x691f59471Bfd2B7d639DCF74671a2d648ED1E331",
+		"0x4Aa5Bf7D840aC607cb5BD3249e6Af6FC86C04897",
+	]),
 	moduleGuards: new Set<Address>([zeroAddress]),
 };
 
-const checkSingleTransaction = or(
+const checkCalls = or(
 	// Generally allow calls to other contracts.
 	and(not(self), exact({ operation: 0 })),
-	// Restrict allowed self calls.
+	// Restrict allowed self-calls.
 	and(
 		self,
 		or(
@@ -48,28 +51,30 @@ const checkSingleTransaction = or(
 			}),
 			call({
 				signature: "function setFallbackHandler(address handler)",
-				args: ([handler]) => ALLOWED.fallbackHandlers.has(handler),
+				args: ([handler]) => SUPPORTED.fallbackHandlers.has(handler),
 			}),
 			call({
 				signature: "function setGuard(address guard)",
-				args: ([guard]) => ALLOWED.guards.has(guard),
+				args: ([guard]) => SUPPORTED.guards.has(guard),
 			}),
 			call({
 				signature: "function enableModule(address module)",
-				args: ([module]) => ALLOWED.modules.has(module),
+				args: ([module]) => SUPPORTED.modules.has(module),
 			}),
 			call({
 				signature: "function disableModule(address prevModule, address module)",
 			}),
 			call({
 				signature: "function setModuleGuard(address guard)",
-				args: ([guard]) => ALLOWED.moduleGuards.has(guard),
+				args: ([guard]) => SUPPORTED.moduleGuards.has(guard),
 			}),
 			receive,
 		),
 	),
-	// Selectively allow delegate calls to:
-	// The SafeMigration contracts:
+);
+
+const checkDelegateCalls = or(
+	// Allow delegate calls to the SafeMigration contracts.
 	and(
 		targets(
 			"0x6439e7ABD8Bb915A5263094784C5CF561c4172AC",
@@ -82,7 +87,7 @@ const checkSingleTransaction = or(
 			delegatecall({ signature: "function migrateL2WithFallbackHandler()" }),
 		),
 	),
-	// The SignMessage contracts:
+	// Allow delegate calls to the SafeMigration contracts.
 	and(
 		targets(
 			"0xA65387F16B013cf2Af4605Ad8aA5ec25a2cbA3a2",
@@ -95,12 +100,40 @@ const checkSingleTransaction = or(
 );
 
 export const checkTransaction = or(
-	checkSingleTransaction,
+	checkCalls,
+	checkDelegateCalls,
 	multisend({
-		to: "0xA83c336B20401Af773B6219BA5027174338D1836",
-		check: checkSingleTransaction,
+		to: "0x218543288004CD07832472D464648173c77D7eB7",
+		check: or(checkCalls, checkDelegateCalls),
 		version: "v1.5.0+",
 	}),
-	multisend({ to: "0x9641d764fc13c8B624c04430C7356C1C7C8102e2", check: checkSingleTransaction }),
-	multisend({ to: "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D", check: checkSingleTransaction }),
+	multisend({
+		to: "0xA83c336B20401Af773B6219BA5027174338D1836",
+		check: checkCalls,
+		version: "v1.5.0+",
+	}),
+	multisend({
+		to: "0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
+		check: or(checkCalls, checkDelegateCalls),
+	}),
+	multisend({
+		to: "0x9641d764fc13c8B624c04430C7356C1C7C8102e2",
+		check: checkCalls,
+	}),
+	multisend({
+		to: "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761",
+		check: or(checkCalls, checkDelegateCalls),
+	}),
+	multisend({
+		to: "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D",
+		check: checkCalls,
+	}),
+	multisend({
+		to: "0x998739BFdAAdde7C933B942a68053933098f9EDa",
+		check: or(checkCalls, checkDelegateCalls),
+	}),
+	multisend({
+		to: "0xA1dabEF33b3B82c7814B6D82A79e50F4AC44102B",
+		check: checkCalls,
+	}),
 );
